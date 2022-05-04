6e9ef9ff957a3d99b539671d0a1f5f2947d93b8d
ADMIN <ndang1336@gmail.com>
2022-05-04 16:25:21 +0700

    Wallpaper_5/4/2022

---
+++ app/src/main/java/com/example/wallpaper/TrangChiTiet.java
@@ -0,0 +1,14 @@
+package com.example.wallpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrangChiTiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trang_chi_tiet);
+    }
+}
